e0298112e502c8380013913f43ee41673a8a5a22
PARSA006 <parsakoulaji6@gmail.com>
2025-10-27 11:09:21 +0200

    initial project

---
+++ Source/Carnival/Carnival.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Carnival.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Carnival, "Carnival" );
+
+DEFINE_LOG_CATEGORY(LogCarnival)
